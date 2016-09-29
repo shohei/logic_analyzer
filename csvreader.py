e9ad894a8374902e342c4769b0c7e0fbc24b1278
Shohei Aoki <shoaok@gmail.com>
2016-09-29 23:56:02 +0900

    add x y z graph

--- csvreader.py
+++ csvreader.py
@@ -2,10 +2,31 @@ from pylab import *
 import matplotlib.pyplot as plt
 import csv 
 
-points = []
+x = []
+y = []
+z = []
 with open('record.csv','r') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
-        points.append(row)
-plt.plot(points)
+        x.append(row[0])
+        y.append(row[1])
+        z.append(row[2])
+
+
+# fig = plt.figure
+
+plt.figure(num=None, figsize=(16, 4), dpi=80, facecolor='w', edgecolor='k')
+
+plt.subplot(131)
+plt.title('x')
+plt.plot(x)
+
+plt.subplot(132)
+plt.title('y')
+plt.plot(y)
+
+plt.subplot(133)
+plt.title('z')
+plt.plot(z)
+
 plt.show()
